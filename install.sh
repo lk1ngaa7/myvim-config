1c3e24882d577f254baf8e8581c314de1249be0d
lk1ngaa7 <lk1ngaa7@gmail.com>
2016-10-04 18:00:07 +0800

    fix

--- install.sh
+++ install.sh
@@ -5,15 +5,15 @@
 # Created Time: Tue Oct  4 17:28:55 2016
 #########################################################################
 #!/bin/bash
-if [!-d "~/.vim"]; then
+if [ ! -d "~/.vim" ]; then
 	mkdir ~/.vim
 fi
-if [!-d "~/.vim/plugin" ]; then
+if [ ! -d "~/.vim/plugin" ]; then
 	mkdir ~/.vim/pulgin
 fi
-if [!-d "~/.vim/doc"]; then
+if [ ! -d "~/.vim/doc" ]; then
 	mkdir ~/.vim/doc
 fi
 cp ~/.vimrc ~/.vimrc_back && cp vimrc ~/.vimrc
-cd solarized && cp -rf ./* ~/.vim/ && cd .. && cd vimcdoc-1.9.0 && sh vimcdoc.sh -i &&cd plugin/NERD_commenter/ && cp -f NERD_commenter.vim ~/.vim/plugin && cp -f NERD_commenter.txt ~/.vim/doc
+cd solarized && cp -rf ./* ~/.vim/ && cd .. && cd vimcdoc-1.9.0 && sh vimcdoc.sh -i && cd .. && cd plugin/NERD_commenter/ && cp -f NERD_commenter.vim ~/.vim/plugin && cp -f NERD_commenter.txt ~/.vim/doc
 
---
+++ test.sh
@@ -0,0 +1,17 @@
+#########################################################################
+# File Name: test.sh
+# Author: lk1ngaa7
+# mail: lk1ngaa7@gmail.com
+# Created Time: Tue Oct  4 17:51:16 2016
+#########################################################################
+#!/bin/bash
+fi
+if [ ! -d "~/.vim/plugin" ]; then
+	mkdir ~/.vim/pulgin
+fi
+if [ ! -d "~/.vim/doc" ]; then
+	mkdir ~/.vim/doc
+fi
+if [ ! -d "/myfolder" ]; then
+	  mkdir /myfolder
+fi
